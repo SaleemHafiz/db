db7ecf9eaa364e8690ffb61bbee26a48c28e9ad3
SaleemHafiz <57508026+SaleemHafiz@users.noreply.github.com>
2025-12-20 03:37:10 +0500

    Rename script from 'LearnQuran Performance Panel' to 'perf'

--- scripts/lq-performances.js
+++ scripts/lq-performances.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         LearnQuran Performance Panel
+// @name         perf
 // @namespace    http://tampermonkey.net/
 // @version      1.4
 // @match        https://my.learnquraan.co.uk/employees/teacher/lesson-step1*
